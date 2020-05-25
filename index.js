f04e6062335191d1476d07a71f5c5564c05fad11
Chahaaaaan <chahaaaaan@gmail.com>
2020-05-25 22:34:56 +0900

    取得した避難場所をHTMLセット

--- index.js
+++ index.js
@@ -12,38 +12,37 @@ document.getElementById("getPositionButton").onclick = function () {
     setData();
 }
 function getEscapePlaces(lati, long) {
-    console.log(lati);
-    console.log(long);
     //座標をいい感じに変換する
-    z = 10 //ズームレベル10固定(国土地理院APIの関係)
-    L = 85.05112878;
-    x = Math.floor((long / 180 + 1) * Math.pow(2, (z + 7)));
-    y = Math.floor((Math.pow(2, (z + 7)) / Math.PI) * (-1 * (Math.atanh(Math.sin(Math.PI * lati / 180))) + Math.atanh(Math.sin(Math.PI * L / 180))));
-    const url = 'http://cyberjapandata.gsi.go.jp/xyz/skhb04/' + z + '/' + Math.floor(x / 256) + '/' + Math.floor(y / 256) + '.geojson';
-    var distance = 99999999999999999999 //どう考えても距離より遠い値入れただけ
-    var siteName = '';
+    const z = 10 //ズームレベル10固定(国土地理院APIの関係)
+    //この辺難しい数学過ぎて正直よくわかってない
+    let L = 85.05112878;
+    let x = Math.floor((long / 180 + 1) * Math.pow(2, (z + 7)));
+    let y = Math.floor((Math.pow(2, (z + 7)) / Math.PI) * (-1 * (Math.atanh(Math.sin(Math.PI * lati / 180))) + Math.atanh(Math.sin(Math.PI * L / 180))));
+    let url = "http://cyberjapandata.gsi.go.jp/xyz/skhb04/" + z + "/" + Math.floor(x / 256) + "/" + Math.floor(y / 256) + ".geojson";
+    var returnObject = { "siteName": "", "x": 0, "y": 0 };
+    let distance = 99999999999999999999 //どう考えても距離より遠い値入れただけ
     fetch(url).then(function (responce) {
         return responce.json();
     }).then(function (geojson) {
         geojson.features.forEach(element => {
-            tmpX = element.geometry.coordinates[1];
-            tmpY = element.geometry.coordinates[0];
-            tmpSiteName = element.properties.name;
-            tmpDistance = Math.sqrt(Math.pow(lati - tmpX, 2) + Math.pow(long - tmpY, 2));
+            let tmpX = element.geometry.coordinates[1];
+            let tmpY = element.geometry.coordinates[0];
+            let tmpSiteName = element.properties.name;
+            let tmpDistance = Math.sqrt(Math.pow(lati - tmpX, 2) + Math.pow(long - tmpY, 2));
             if (distance > tmpDistance) {
-                siteName = tmpSiteName;
+                returnObject.siteName = tmpSiteName;
                 distance = tmpDistance;
+                returnObject.x= tmpX;
+                returnObject.y = tmpY;
             }
         })
-    }).then(() => {
-        console.log(distance);
-        console.log(siteName);
+        return returnObject;
+    }).then((obj) => {
+        document.getElementById("siteName").innerHTML = obj.siteName;
+        console.log(obj.x);
+        console.log(obj.y);
     });
-    //TODO 国土地理院と和解してgeojsonを返す
-}
-function findNearestPlace(geojson) {
-    //TODO geojsonから最も近い場所を見つける
-    //object{latitude:緯度 longitude:東経 name:場所名}
+    //setEscapeRootに座標を渡すので、obj.latitude/obj.longitudeを返す
 }
 function setEscapeRoot(PrLat, PrLon, DnLat, DnLon) {
     //TODO GM APIを叩いて最短距離を求め、マップに設定
@@ -52,6 +51,6 @@ function positionsOK(positions) {
     const presentLatitude = positions.coords.latitude;
     const presentLongitude = positions.coords.longitude;
     //placeには名前、座標が必要
-    const placedata = findNearestPlace(getEscapePlaces(presentLatitude, presentLongitude));
+    const placedata = getEscapePlaces(presentLatitude, presentLongitude);
     setEscapeRoot(presentLatitude, presentLongitude, placedata.latitude, placedata.longitude);
 }
