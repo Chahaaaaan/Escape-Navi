84a6b0d8dd11fe95539350097c38f0816eb8511c
Chahaaaaan <chahaaaaan@gmail.com>
2020-05-25 21:15:55 +0900

    座標変換を実装

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ function setData() {
 document.getElementById("getPositionButton").onclick = function () {
     setData();
 }
-function getEscapePlaces(lati,long) {
+function getEscapePlaces(lati, long) {
+    console.log(lati);
+    console.log(long);
+    //座標をいい感じに変換する
+    z = 10 //ズームレベル10固定(国土地理院APIの関係)
+    L = 85.05112878;
+    x = Math.floor((long / 180 + 1) * Math.pow(2, (z + 7)));
+    y = Math.floor((Math.pow(2, (z + 7)) / Math.PI) * (-1 * (Math.atanh(Math.sin(Math.PI * lati / 180))) + Math.atanh(Math.sin(Math.PI * L / 180))));
     //TODO 国土地理院と和解してgeojsonを返す
 }
 function findNearestPlace(geojson) {
     //TODO geojsonから最も近い場所を見つける
     //object{latitude:緯度 longitude:東経 name:場所名}
 }
-function setEscapeRoot(PrLat,PrLon,DnLat,DnLon) {
+function setEscapeRoot(PrLat, PrLon, DnLat, DnLon) {
     //TODO GM APIを叩いて最短距離を求め、マップに設定
 }
 function positionsOK(positions) {
